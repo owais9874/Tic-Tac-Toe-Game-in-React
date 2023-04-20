e5c6ed7c19190ca1a674b9fe68ffc4b74c44dace
Owais <mowais9874@gmail.com>
2023-04-20 15:39:57 +0500

    Update

--- src/TicTacToeGame/SquareBox/Square.js
+++ src/TicTacToeGame/SquareBox/Square.js
@@ -1,22 +1,18 @@
 import React from "react";
 
-const boxStyle ={
+const boxStyle = {
   background: "lightblue",
   border: "1px solid",
   height: "100px",
   width: "300px",
   display: "flex",
   justifyContent: "center",
-  alignItems: "center"
-}
+  alignItems: "center",
+};
 
 const Square = (props) => {
   return (
-    <div
-      onClick={props.onClick}
-      style={boxStyle}
-      className="square"
-    >
+    <div onClick={props.onClick} style={boxStyle} className="square">
       <h5>{props.value}</h5>
     </div>
   );
